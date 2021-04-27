867619494275c63b81c160cf8376ee5fed4105fd
morfimgabriel <morfimgabriel@hotmail.com>
2021-04-27 19:24:10 -0300

    Criado pagina dos episodes por id

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { GetStaticProps } from 'next';
 import { api } from '../services/api';
 import Image from 'next/image';
+import Link from 'next/link';
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
@@ -48,7 +49,10 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                 />
 
                 <div className={styles.episodeDetails}>
-                  <a href="">{episode.title}</a>
+                  <Link href={`/episodes/${episode.id}`}>
+                    <a>{episode.title}</a>
+                  </Link>
+
                   <p>{episode.members}</p>
                   <span> {episode.publishedAt} </span>
                   <span> {episode.durationAsString} </span>
@@ -69,18 +73,20 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
 
         <table cellSpacing={0}>
           <thead>
-            <th></th>
-            <th>Podcast</th>
-            <th>Integrantes</th>
-            <th>Data</th>
-            <th>Duração</th>
-            <th></th>
+            <tr>
+              <th></th>
+              <th>Podcast</th>
+              <th>Integrantes</th>
+              <th>Data</th>
+              <th>Duração</th>
+              <th></th>
+            </tr>
           </thead>
           <tbody>
             {allEpisodes.map(episode => {
               return (
                 <tr key={episode.id}>
-                  <td>
+                  <td style={{ width: 72 }}>
                     <Image
                       width={120}
                       height={120}
@@ -89,10 +95,12 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                       objectFit="cover" />
                   </td>
                   <td>
-                    <a href="">{episode.title}</a>
+                    <Link href={`/episodes/${episode.id}`}>
+                      <a>{episode.title}</a>
+                    </Link>
                   </td>
                   <td> {episode.members} </td>
-                  <td> {episode.publishedAt} </td>
+                  <td style={{ width: 100 }}> {episode.publishedAt} </td>
                   <td> {episode.durationAsString} </td>
                   <td>
                     <button type="button">
